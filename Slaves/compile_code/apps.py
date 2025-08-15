57955ca52cf205d38248109b2ddad0da11f4bf97
Vyom Shah <vyom509shah@gmail.com>
2025-08-16 02:26:16 +0530

    Test Check 2

--- Slaves/compile_code/apps.py
+++ Slaves/compile_code/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CompileCodeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Slaves.compile_code"
+    name = "compile_code"
